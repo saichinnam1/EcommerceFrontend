607934ab93474c5a8da142964645d6f01356347c
saichinnam1 <saichinnam566@gmail.com>
2025-05-10 19:07:59 +0530

    updated _redirectfile in public

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,2 +1,2 @@
-export const API_URL = process.env.REACT_APP_API_URL || 'https://technobackend1.onrender.com/api';
+export const API_URL = process.env.REACT_APP_API_URL || 'https://flexible-austine-saichinnam1-d486831b.koyeb.app/api';
 export const BASE_URL = API_URL.replace('/api', '');
